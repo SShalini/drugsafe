8189c4284436af7ca405cdf1231c226c5a12193e
Prashant Singh <prashant12it@gmail.com>
2016-10-21 17:52:50 +0530

    prashant v1

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+//echo 'hello';
 if(ENVIRONMENT == 'development'){
     $config['base_url'] = 'http://local.drugsafe.com/';
 }elseif (ENVIRONMENT == 'testing'){
