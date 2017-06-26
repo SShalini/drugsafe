5b866e0b4a808947e21d8b5bd7619d8ff1a386a2
Prashant Singh <prashant12it@gmail.com>
2017-06-26 11:33:21 +0530

    Docket pdf name change.
    Signed-off-by: Prashant Singh <prashant12it@gmail.com>

--- application/controllers/Order_Controller.php
+++ application/controllers/Order_Controller.php
@@ -567,9 +567,9 @@ EOD;
         $this->load->library('Pdf');
         $pdf = new Pdf('L', 'mm', 'A4', true, 'UTF-8', false);
         $pdf->SetCreator(PDF_CREATOR);
-        $pdf->SetTitle('Drug-safe Order Details');
+        $pdf->SetTitle('Drug-safe Delivery Docket');
         $pdf->SetAuthor('Drug-safe');
-        $pdf->SetSubject('Order Details Report PDF');
+        $pdf->SetSubject(' Delivery Docket PDF');
         $pdf->SetMargins(PDF_MARGIN_LEFT - 10, PDF_MARGIN_TOP - 18, PDF_MARGIN_RIGHT - 10);
         $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
         $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
@@ -697,7 +697,7 @@ EOD;
 	$pdf->writeHTML($tbl, true, false, true, false, '');
         error_reporting(E_ALL);
         ob_end_clean();
-       $pdf->Output('view_order_details.pdf', 'I');
+       $pdf->Output('view_delivery_docket.pdf', 'I');
     }
     function View_excel_order_details_data()
         {
